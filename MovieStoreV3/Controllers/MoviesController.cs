b84eb161fabe99541995400dc0ddbd818ca091f5
Alex Hawley <asimo124@yahoo.com>
2021-01-01 22:12:01 -0600

    - First Commit

---
+++ MovieStoreV3/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using MovieStoreV3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieStoreV3.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: movies/random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            return View(movie);
+        }
+    }
+}
---
+++ MovieStoreV3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MovieStoreV3.Startup))]
+namespace MovieStoreV3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
