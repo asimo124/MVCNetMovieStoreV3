64ce38a5b7a5dfde60f2eb77af9f93a76ee67e17
Alex Hawley <asimo124@yahoo.com>
2021-01-01 23:31:02 -0600

    - Working on Layout with Bootstrap Lumen Theme

--- MovieStoreV3/App_Start/BundleConfig.cs
+++ MovieStoreV3/App_Start/BundleConfig.cs
@@ -1,13 +1,18 @@
 ﻿using System.Web;
 using System.Web.Optimization;
 
+
+
 namespace MovieStoreV3
 {
     public class BundleConfig
     {
+
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            BundleTable.EnableOptimizations = true;
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -23,8 +28,10 @@ namespace MovieStoreV3
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-lumen.css",
                       "~/Content/site.css"));
+
+            bundles.IgnoreList.Clear();
         }
     }
 }
